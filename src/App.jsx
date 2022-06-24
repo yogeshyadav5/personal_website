1546a1b29e4bc4d3c1f0d8c3c0a37043840873a2
Yogesh Yadav <yogesh.yadavyj@gmail.com>
2022-06-24 15:17:48 +0530

    persoanl website

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/About/About';
+import Experience from './components/Experience/Experience';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
+import Footer from './components/footer/Footer';
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Portfolio />
+    <Contact />
+    <Footer />
+
+    </>
+  );
+}
+
+export default App;
+
